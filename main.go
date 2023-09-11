a6256de9ab5664eab7b68861bb73a6bc5bb619f9
Wslmacieira <wslmacieira@outlook.com>
2023-09-10 22:19:29 -0300

    feat: adicionando logs ao projeto

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/wsldev/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/wsldev/meu-primeiro-crud-go/src/controller/routes"
 )
 
 func main() {
+	logger.Info("About to start user application")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,34 +1,40 @@
 package controller
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/wsldev/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/wsldev/meu-primeiro-crud-go/src/configuration/validation"
 	"github.com/wsldev/meu-primeiro-crud-go/src/controller/model/request"
 	"github.com/wsldev/meu-primeiro-crud-go/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"))
 
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("Error trying to marshal object, error=%s", err.Error())
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
+
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println(userRequest)
 	response := response.UserResponse{
 		Email: userRequest.Email,
 		ID:    "test",
 		Name:  userRequest.Name,
 		Age:   userRequest.Age,
 	}
+
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"))
 
 	c.JSON(http.StatusCreated, response)
 }
