bf83773dda34e3f51c95eb436673edadc916ac17
Wslmacieira <wslmacieira@outlook.com>
2023-09-10 21:27:05 -0300

    feat: personalizando erros de validação

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,11 +2,13 @@ package controller
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/wsldev/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/wsldev/meu-primeiro-crud-go/src/configuration/validation"
 	"github.com/wsldev/meu-primeiro-crud-go/src/controller/model/request"
+	"github.com/wsldev/meu-primeiro-crud-go/src/controller/model/response"
 )
 
 func CreateUser(c *gin.Context) {
@@ -14,12 +16,19 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			"There are some incorrect fields")
+		log.Printf("Error trying to marshal object, error=%s", err.Error())
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
-		fmt.Println(err.Error())
 		return
 	}
 
-	c.JSON(http.StatusCreated, userRequest)
+	fmt.Println(userRequest)
+	response := response.UserResponse{
+		Email: userRequest.Email,
+		ID:    "test",
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(http.StatusCreated, response)
 }
